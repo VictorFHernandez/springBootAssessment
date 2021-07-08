44ea8cbc44fa67782f7f472f4c24e8cceae18d2a
VictorFHernandez <victorfhernandez3@gmail.com>
2021-07-07 20:27:39 -0500

    added the id,email,total_price, constructors, getters and seeters to the Order Model

--- src/main/java/com/codeup/springbootassessment/models/Order.java
+++ src/main/java/com/codeup/springbootassessment/models/Order.java
@@ -1,8 +1,58 @@
 package com.codeup.springbootassessment.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.aspectj.weaver.ast.Or;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private double total_price;
+
+    public Order(){
+
+    }
+
+    public Order(String email, double total_price){
+        this.email = email;
+        this.total_price = total_price;
+    }
+
+    public Order(long id, String email, double total_price){
+        this.id = id;
+        this.email = email;
+        this.total_price = total_price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(double total_price) {
+        this.total_price = total_price;
+    }
 }

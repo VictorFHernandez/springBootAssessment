05c094786738b9322b62b807a6fc73e9d744e077
VictorFHernandez <victorfhernandez3@gmail.com>
2021-07-07 20:14:14 -0500

    added the database credentials to the application.properties file and added the libraries to the pom file and created a assessment_user

--- src/main/java/com/codeup/springbootassessment/MySQL/Migration.sql
+++ src/main/java/com/codeup/springbootassessment/MySQL/Migration.sql
@@ -3,6 +3,9 @@ CREATE DATABASE IF NOT EXISTS springassessment_db;
 
 USE springassessment_db;
 
+CREATE USER IF NOT EXISTS assessment_user@localhost IDENTIFIED BY 'codeup';
+GRANT ALL ON springassessment_db.* TO assessment_user@localhost;
+
 CREATE TABLE IF NOT EXISTS orders (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     email VARCHAR (255) NOT NULL,

d07a15b63ecc2e558bc7b82d0e297469f3c5ef0f
VictorFHernandez <victorfhernandez3@gmail.com>
2021-07-08 00:26:40 -0500

    completed the delete function on the ordersController and created a create.html and a show.html

--- src/main/java/com/codeup/springbootassessment/controllers/OrderController.java
+++ src/main/java/com/codeup/springbootassessment/controllers/OrderController.java
@@ -1,4 +1,48 @@
 package com.codeup.springbootassessment.controllers;
 
+import com.codeup.springbootassessment.daos.OrderRepository;
+import com.codeup.springbootassessment.models.Order;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class OrderController {
+    private OrderRepository orderDao;
+
+    public OrderController(OrderRepository orderDao){
+        this.orderDao = orderDao;
+    }
+
+    @GetMapping("/orders/create")
+    @ResponseBody
+    public String showCreateOrderForm(Model model){
+        model.addAttribute("order", new Order());
+        return "orders/create";
+    }
+
+//    @PostMapping("/orders/create")
+//    public String createOrder(@ModelAttribute Order order){
+//        order.setId(order.getId());
+//
+//    }
+
+//    @GetMapping("/orders/{id}"){
+//
+//}
+
+//    @GetMapping("/orders/{id}/update"){
+//
+//}
+
+//    @PostMapping("/orders/{id}/update"){
+//
+//    }
+
+    @PostMapping("/orders/{id}/delete")
+    public String ddeleteOrder(@PathVariable long id){
+        orderDao.deleteById(id);
+        return "redirect:/orders";
+    }
+
 }
--- src/main/java/com/codeup/springbootassessment/daos/OrderRepository.java
+++ src/main/java/com/codeup/springbootassessment/daos/OrderRepository.java
@@ -1,4 +1,13 @@
 package com.codeup.springbootassessment.daos;
 
-public interface OrderRepository {
+
+import com.codeup.springbootassessment.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<OrderRepository, Long> {
+    List<Order> findAllById (long id);
+    Order findAllByEmail(String email);
+    Order findAllByTotal_price (double total_price);
 }
